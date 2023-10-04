9af22d46f85a3e3c60c96410beb0215261cd9c54
ADE <jeffersonmeffun@gmail.com>
2023-10-03 23:29:41 -0700

    fix file 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-for char in range(ord('z'), ord('A') - 1, -1):
-    if char < ord('a') and char > ord('Z'):
-        continue
-    print("{:c}".format(char if char % 2 == 0 else char - 32), end="")
+def print_tebahpla():
+    alphabet = ""
+    for i in range(122, 96, -1):
+        if i % 2 == 0:
+            alphabet += chr(i).lower()
+        else:
+            alphabet += chr(i).upper()
+    print(alphabet, end="")
+
+
+print_tebahpla()
