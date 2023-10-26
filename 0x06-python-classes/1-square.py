808b305762bd95b9f006cd172684f90b9da30316
ADE <theking-ade@outlook.com>
2023-10-25 18:47:15 -0700

    add file 1-..

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This is a module that defines a square class.
+"""
+
+class Square:
+    """
+    This is a class that represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
