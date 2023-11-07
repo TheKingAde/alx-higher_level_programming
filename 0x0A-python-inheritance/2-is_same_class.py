377813eba9ad1ed30ee1d1a9bbbb579b8f63e169
ADE <theking-ade@outlook.com>
2023-11-06 19:20:01 -0800

    add file 2-is_same_class n test

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Define class is_same_class'''
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if the object is exactly an instance of the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if the object is exactly an instance of the specified class; otherwise, False.
+    """
+    return type(obj) is a_class
