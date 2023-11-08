e37a75c0832f224c812621a993ad83613b6edd64
ADE <theking-ade@outlook.com>
2023-11-07 20:34:23 -0800

    add file 9

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """defines a function that adds all arguments to a Python list"""
+
+
 import sys
 from os import path
 from 5-save_to_json_file import save_to_json_file
@@ -7,7 +9,16 @@ from 6-load_from_json_file import load_from_json_file
 
 
 def add_items_to_list_and_save(args, filename):
-    """adds all arguments to a Python list"""
+    """
+    Add command-line arguments to a Python list and save it to a JSON file.
+
+    Args:
+        args (list): A list of command-line arguments to be added.
+        filename (str): The name of the JSON file for saving the list.
+
+    Returns:
+        None
+    """
     if path.exists(filename):
         my_list = load_from_json_file(filename)
     else:
