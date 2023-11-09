1e997d284f2219fbb410e65328aef0c8ef7a15a9
ADE <theking-ade@outlook.com>
2023-11-08 16:59:32 -0800

    add file 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Defines a function that raises an Exception"""
+
+
+class BaseGeometry:
+    """This is the BaseGeometry class."""
+    def area(self):
+        """Calculate the area of the geometric shape"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates the integer value.
+
+        Args:
+            name(str): name of value being evaluated
+            value(int): value to be validated.
+
+        Raises:
+            TypeError and ValueError
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Represents a geometric rectangle"""
+    def __init__(self, width, height):
+        """Initialize a Rectangle instance with width and height"""
+        self.__width = 0
+        self.__height = 0
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """
+        Calculate and return the area of a triangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        Return a string representation of the triangle
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """
+    Represents a geometric square
+    """
+    def __init__(self, size):
+        """
+        Initialize a square distance
+
+        Args:
+            size (int): The size of the square
+        """
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        Returns the string representation of the square
+        """
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
