166c55357207d3da5ad844f8ee9d85010dc62f79
ADE <theking-ade@outlook.com>
2023-12-15 07:00:29 -0800

    fix other files

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- script that lists all records of the table second_table of the database
-SELECT 'score', 'name'
-FROM 'second_table' 
-WHERE 'name' != ""
-ORDER BY 'score' DESC;
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC;
