a27fddb306ca88263cd08657a76a800719e5a097
ADE <theking-ade@outlook.com>
2023-12-15 09:37:07 -0800

    add file 0-privileges.sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,7 @@
+-- Lists all shows in the database hbtn_0d_tvshows without a genre linked.
+SELECT `title`, SUM(`rate`) AS `rating`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_ratings` AS r
+       ON t.`id` = r.`show_id`
+ GROUP BY `title`
+ ORDER BY `rating` DESC;
