18d13e1b32559d9629c0bc91faffa7ca20206acd
ADE <theking-ade@outlook.com>
2023-12-16 18:28:16 -0800

    add file 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,6 @@
 -- create the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS `htbn_0d_usa`;
-USE `htbn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `states` (
+CREATE TABLE IF NOT EXISTS `htbn_0d_usa`.`states` (
 	`id` INT AUTO_INCREMENT,
 	`name` VARCHAR(256) NOT NULL,
 	PRIMARY KEY (`id`)
