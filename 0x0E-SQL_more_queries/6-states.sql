5d813728790ca92b089b8b72d9bf1d8dc9bc41ce
ADE <theking-ade@outlook.com>
2023-12-16 18:41:26 -0800

    add file 7-cities.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS `htbn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `htbn_0d_usa`.`states` (
 	PRIMARY KEY(`id`),
-	`id` INT            NOT NULL AUTO_INCREMENT,
+	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(256) NOT NULL
 );
