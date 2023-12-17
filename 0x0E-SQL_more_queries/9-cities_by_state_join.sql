3995633617da828bb531a1f3dc3df32632708ae0
ADE <theking-ade@outlook.com>
2023-12-16 18:58:51 -0800

    add file 9-cities_by_state_join.sql

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- lists all cities contained in the database htbn_0d_usa
+SELECT cities.id, cities.name, (SELECT states.name FROM states WHERE cities.state_id = states.id) AS state_name
+FROM cities
+ORDER BY cities.id ASC;
