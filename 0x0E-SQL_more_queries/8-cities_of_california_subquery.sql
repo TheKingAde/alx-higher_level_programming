15294508fac7dabbf7d236871910dfc39137419e
ADE <theking-ade@outlook.com>
2023-12-16 18:55:47 -0800

    fix 8-cities_of...

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,8 @@
 -- lists all the cities of California that can be found in the database htbn_0d_usa
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
