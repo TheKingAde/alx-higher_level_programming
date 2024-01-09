f3dab5d23837d64181873d65353813c127a2ab91
ADE <theking-ade@outlook.com>
2024-01-08 23:17:04 -0800

    add file 11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const nums = process.argv.slice(2).map(Number);
+	const maxNum = Math.max(...nums);
+	console.log(Math.max(...nums.filter(num => num !== maxNum)));
+}
