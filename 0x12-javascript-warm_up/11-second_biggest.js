33991321a1b6e86dac93fd3d8671a1b7f027964a
ADE <theking-ade@outlook.com>
2024-01-08 23:32:13 -0800

    fix *.js (semistandard)

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,6 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   const nums = process.argv.slice(2).map(Number);
-	const maxNum = Math.max(...nums);
-	console.log(Math.max(...nums.filter(num => num !== maxNum)));
+  const maxNum = Math.max(...nums);
+  console.log(Math.max(...nums.filter(num => num !== maxNum)));
 }
