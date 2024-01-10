68ad7d4d6b98ee1b2e066cfd73c47f7ae877ea7c
ADE <theking-ade@outlook.com>
2024-01-09 20:55:49 -0800

    add file 5-square.js

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Square;
