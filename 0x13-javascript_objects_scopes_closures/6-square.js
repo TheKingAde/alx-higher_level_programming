4c9ffd5ad99dbf55e662ac1b079bcdaafe7cf843
ADE <theking-ade@outlook.com>
2024-01-09 21:16:43 -0800

    add file 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
-const Square = required('./5-square');
-class Square extends Square {
-  constructor(size) {
-    super(size);
-  }
-
-  charPrint(c) {
-    if (c == undefined) {
+const Square5 = require('./5-square');
+class Square extends Square5 {
+  charPrint (c) {
+    if (c === undefined) {
       c = 'X';
     }
 
-    for (let = 0; i < this.height; i++) {
+    for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
   }
