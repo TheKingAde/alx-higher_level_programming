de895445a089a4d557cc7ce5bbe1c5381eebccd8
ADE <theking-ade@outlook.com>
2024-01-27 00:11:57 -0800

    update file 6-peak.txt and 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 # Make a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!"
 curl -s -L -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
