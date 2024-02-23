e89dd15a3ae37db1e3b261649c6e52d3d1e26532
KingAde <theking-ade@outlook.com>
2024-02-22 21:44:36 -0800

    add file 100 and 101

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const todos = JSON.parse(body);
+    const tasksCompleted = {};
+
+    for (const task of todos) {
+      if (task.completed) {
+        if (tasksCompleted[task.userId]) {
+          tasksCompleted[task.userId]++;
+        } else {
+          tasksCompleted[task.userId] = 1;
+        }
+      }
+    }
+
+    console.log(tasksCompleted);
+  }
+});
